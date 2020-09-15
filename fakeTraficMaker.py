ba632e6eb17e76faa0cbffe6b145c2386af4511c
KanishkRanjan <68316017+KanishkRanjan@users.noreply.github.com>
2020-09-15 19:04:22 +0530

    Update fakeTraficMaker.py
    You need to install tor and set ctrl+shift+t to open tor

--- fakeTraficMaker.py
+++ fakeTraficMaker.py
@@ -1,6 +1,89 @@
-import os
+import pyautogui as pyg
+import time
+import random
+import time
+from datetime import datetime
 
-os.chdir(r'C:\Users\User\Desktop\Blog')
 
-os.system("pythonw.exe .\exercise.py")
-quit()
+
+clicked = 0
+currentTime = str(datetime.now())
+timeIntervals = [240,360,120,60,50,30,10]
+
+timeIntervals =[5,6,7]
+def actHuman(x):
+    selectedTime = random.choice(timeIntervals)
+    strSelectedTime = str(selectedTime)
+    pyg.scroll(x)
+    time.sleep(selectedTime)
+    print('Times and the time interval was ',end='')
+    print(strSelectedTime)
+    
+# change url here
+
+url = "https://homeage17.blogspot.com/2020/06/blog-post.html"
+logFile = "log.txt" #log file name
+
+f = open(logFile, "a")
+f.write('\n----------------------------')
+f.write(currentTime)
+f.write('----------------------------\n')
+f.close()
+
+#opening browser
+
+pyg.hotkey('ctrl','shift','t')
+time.sleep(15)
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------Capnin----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+while 1:
+    
+# veriable
+    time.sleep(2)
+    clicked +=1
+    strClicked = str(clicked)
+
+    
+#tying  search
+    
+    pyg.press('f6')
+    pyg.write(url)
+    time.sleep(0.5)
+    pyg.press('enter')
+ 
+#scrolling page
+
+    pyg.moveTo(373,683)
+    time.sleep(10)
+
+    actHuman(-10000)
+    actHuman(-10000)
+    actHuman(-10000)
+    
+    pyg.scroll(-10000)
+    time.sleep(3)
+    pyg.scroll(10000)
+
+#changing ip address
+    
+    pyg.hotkey('ctrl', 'shift','u') #creating new identy in tor
+    time.sleep(3)
+
+#adding in log
+
+    f = open(logFile, "a")
+    f.write('\nThis link was clicked ')
+    f.write(strClicked)
+    f.write('\n')
+    f.close()
+
+# print on terminal
+
+    print('went over the page ' ,end='')
+    print(clicked)
+ 
+
+#--------------this was made by capnin and uploaded on github---------------
